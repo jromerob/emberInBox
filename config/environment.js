bf15d8ce1f4ace96b7d244bc5734c45c4b1af462
0011188 <0011188@PC02EY1P.insags.com>
2016-08-16 09:37:32 +0200

    Configuracion

--- config/environment.js
+++ config/environment.js
@@ -41,6 +41,20 @@ module.exports = function(environment) {
   if (environment === 'production') {
 
   }
+  
+  /* configuracion para el uso de fuentes de google fonts */
+  
+  ENV.contentSecurityPolicy = {
+  'default-src': "'none'",
+  'script-src': "'self' 'unsafe-inline'",
+  'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
+  'font-src': "'self' fonts.gstatic.com",
+  'connect-src': "'self'",
+  'img-src': "'self' data:",
+  'media-src': "'self'"
+}
 
   return ENV;
 };
+
+
